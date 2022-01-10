bad7b88e0df59c4fc92f80eecf1e21a5702782ba
antoniolpcan <antoniolpctwice@gmail.com>
2022-01-10 16:37:15 -0300

    change structure 3

--- routers/imports.py
+++ routers/imports.py
@@ -4,5 +4,5 @@ from fastapi.middleware.cors import CORSMiddleware
 import pandas as pd
 from typing import List, Union
 from definitions.in_definitions import *
-from functions.in_functions import *
+from functions.in_functions import *       
 from security.auth import get_tok       
